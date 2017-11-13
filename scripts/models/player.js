e2abaab7955d2d5c7bb08f9cc459854761fd582d
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-13 15:08:46 -0800

    constructor works, data access works

--- scripts/models/player.js
+++ scripts/models/player.js
@@ -4,24 +4,22 @@
 
 var __API_URL__ = 'https://owjs.ovh'
 
-let user = [];
+let players = [];
 
-let loadUser = function (ctx) {
-  $.ajax({
-    url: `${__API_URL__}/all/pc/us/CheshireKat-1372`,
-    method: 'GET'
-  })
-    .then(results => user = results[0])
-  err => console.error(err.status, err.statusText);
+function Player(data) {
+  console.log('constructor triggered');
+  this.profile = data.profile,
+  this.quickplay = data.quickplay.global,
+  this.heroes = data.quickplay.heroes,
+  this.achievements = data.achievements
+  players.push(this);
+}
+
+
+let loadUser = function () {
+  $.get(`${__API_URL__}/all/pc/us/CheshireKat-1372`, function (data) {
+    new Player(data);
+  });
 }
 
 loadUser();
-// let user = [];
-
-// let test = function () {
-// $.get(`${__API_URL__}/all/pc/us/CheshireKat-1372`, function (data, status) {
-//   console.log(data, status);
-// });
-// }
-
-// test();
