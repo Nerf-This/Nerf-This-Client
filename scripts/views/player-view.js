8418338003a3b164a2c58760077fe1f2a85ffb8a
William Johnson <williamgjohnson@live.com>
2017-11-14 09:23:00 -0800

    Work on routes and player-view

---
+++ scripts/views/player-view.js
@@ -0,0 +1,33 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const playerView = {};
+
+  function reset() {
+    $('.container').hide();
+    $('navigation').slideDown(350);
+  }
+  //TODO: Flesh out initIndexPage
+  playerView.initIndexPage = function() {
+    reset();
+    $('.search').show();
+  }
+  //TODO: Flesh out initAboutPage
+  playerView.initAboutPage = function() {
+    reset();
+    $('.about').show();
+  }
+  //TODO: Flesh out initAchievementPage
+  playerView.initAchievementPage = function() {
+    reset();
+    $('.achievement-view').show();
+  }
+
+  // playerView.initTeamPage = function() {
+  //   reset();
+  //   $('.team-link').show();
+  // }
+
+  module.playerView = playerView;
+})(app)
--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -4,3 +4,8 @@ if (window.location.pathname !== '/') {
   console.log('Running remote.  Setting base to /Nerf-This');
   page.base('/Nerf-This');
 }
+
+page('/', ctx => app.Player.fetchAll(app.playerView.initIndexPage));
+page('/achievement-comparison', () => app.Player.fetchAll(app.playerView.initAchievementPage));
+//page('/team-link', ctx => app.Player.fetchAll(app.playerView.initTeamPage));
+page('/about', () => app.playerView.initAboutPage());
