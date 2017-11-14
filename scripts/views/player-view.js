96fe2c964be332377950288b3a7cfe1e5d5f6a30
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-14 12:00:19 -0800

    routing works

--- scripts/views/player-view.js
+++ scripts/views/player-view.js
@@ -6,29 +6,33 @@ var app = app || {};
   const playerView = {};
 
   function reset() {
+    console.log('reset triggered');
     $('.container').hide();
     $('navigation').slideDown(350);
   }
   //TODO: Flesh out initIndexPage
   playerView.initIndexPage = function() {
+    console.log('initindexpage triggered');
     reset();
     $('.search').show();
   }
   //TODO: Flesh out initAboutPage
   playerView.initAboutPage = function() {
+    console.log('initaboutpage triggered');
     reset();
     $('.about').show();
   }
   //TODO: Flesh out initAchievementPage
   playerView.initAchievementPage = function() {
+    console.log('initachievementpage triggered');
     reset();
     $('.achievement-view').show();
   }
 
-  // playerView.initTeamPage = function() {
-  //   reset();
-  //   $('.team-link').show();
-  // }
+  playerView.initTeamPage = function() {
+    reset();
+    $('.team-link').show();
+  }
 
   module.playerView = playerView;
 })(app)
--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -5,7 +5,9 @@ if (window.location.pathname !== '/') {
   page.base('/Nerf-This');
 }
 
-page('/', ctx => app.Player.fetchAll(app.playerView.initIndexPage));
-page('/achievement-comparison', () => app.Player.fetchAll(app.playerView.initAchievementPage));
-//page('/team-link', ctx => app.Player.fetchAll(app.playerView.initTeamPage));
-page('/about', () => app.playerView.initAboutPage());
+page('/', app.playerView.initIndexPage);
+page('/achievement-comparison', app.playerView.initAchievementPage);
+page('/team-link', app.playerView.initTeamPage);
+page('/about', app.playerView.initAboutPage);
+
+page();
