b949906a43981f0192a0d84259c3977d5ab2e778
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-15 10:19:00 -0800

    made function blind to user

--- scripts/models/player.js
+++ scripts/models/player.js
@@ -3,15 +3,14 @@
 var app = app || {};
 var __API_URL__ = 'https://owjs.ovh';
 let allPlayers = [];
-let primaryHeroes = [];
-let secondaryHeroes = [];
-let primaryHeroHours = [];
-let secondaryHeroHours = [];
+// let primaryHeroes = [];
+// let secondaryHeroes = [];
+// let primaryHeroHours = [];
+// let secondaryHeroHours = [];
 
 (function(module) {
   //Player constructor function
   function Player(data) {
-    console.log('constructor triggered');
     this.profile = data.profile,
     this.quickplay = data.quickplay.global,
     this.heroes = data.quickplay.heroes,
@@ -25,9 +24,7 @@ let secondaryHeroHours = [];
     $.get(`${__API_URL__}/all/${platform}/${region}/${battletag}`, function (data) {
       allPlayers[0] = new Player(data);
       primaryHeroes = Object.keys(allPlayers[0].heroes);
-      primaryHeroHours = heroHours();
-      console.log(primaryHeroHours);
-      console.log('inside api call');
+      primaryHeroHours = heroHours(0);
     })
       .then(app.playerView.initPlayerPage);
   }
@@ -37,7 +34,7 @@ let secondaryHeroHours = [];
     $.get(`${__API_URL__}/all/${platform}/${region}/${battletag}`, function (data){
       allPlayers[1] = new Player(data);
       secondaryHeroes = Object.keys(allPlayers[1].heroes);
-      console.log('Inside the compare player API call');
+      primaryHeroHours = heroHours(1);
     })
       .then(app.playerView.initComparePage);
   }
@@ -48,7 +45,6 @@ let secondaryHeroHours = [];
       let platform = event.target.platform.value.toLowerCase();
       let region = event.target.region.value.toLowerCase();
       let battletag = event.target.battletag.value.split('#').join('-');
-      console.log(battletag, region, platform);
       Player.loadPlayer(platform, region, battletag);
     })
   }
@@ -58,17 +54,15 @@ let secondaryHeroHours = [];
       let platform = event.target.platform.value.toLowerCase();
       let region = event.target.region.value.toLowerCase();
       let battletag = event.target.battletag.value.split('#').join('-');
-      console.log(platform, region, battletag);
       Player.comparePlayer(platform, region, battletag);
     })
   }
 
   //Getting an array of hours per hero
-
-  function heroHours() {
+  function heroHours(player) {
     let time = [];
-    for(var key in allPlayers[0].heroes){
-      time.push(allPlayers[0].heroes[key].time_played/3600000)
+    for(var key in allPlayers[player].heroes){
+      time.push(Math.ceil(allPlayers[player].heroes[key].time_played/3600000))
     }
     return time;
   }
