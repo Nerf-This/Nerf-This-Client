a26a38828289a42a1e601a3988e0cf2cf4619918
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-14 18:25:41 -0800

    trying to get hours per hero

--- scripts/models/player.js
+++ scripts/models/player.js
@@ -19,16 +19,18 @@ let secondaryHeroHours = [];
     this.totalHours = data.quickplay.global.time_played/3600000
   }
 
-  //API call for player data
+  //API call for primary player data
   Player.loadPlayer = function (platform, region, battletag) {
     console.log(battletag);
     $.get(`${__API_URL__}/all/${platform}/${region}/${battletag}`, function (data) {
       allPlayers[0] = new Player(data);
       primaryHeroes = Object.keys(allPlayers[0].heroes);
+      primaryHeroHours = heroHours(primaryHeroes, allPlayers[0].heroes);
       console.log('inside api call');
     })
       .then(app.playerView.initPlayerPage);
   }
+  //API call for a player to compare against
   Player.comparePlayer = function (platform, region, battletag) {
     console.log(battletag);
     $.get(`${__API_URL__}/all/${platform}/${region}/${battletag}`, function (data){
@@ -38,6 +40,7 @@ let secondaryHeroHours = [];
     })
       .then(app.playerView.initComparePage);
   }
+  //Getting battletag info from forms
   Player.getPlayer = function () {
     $('#primary-tag').on('submit', function(event) {
       event.preventDefault();
@@ -58,6 +61,30 @@ let secondaryHeroHours = [];
       Player.comparePlayer(platform, region, battletag);
     })
   }
+
+  //Trying to get an array of hours per hero, using the array of hero names
+  // function heroHours(a, b) {
+  //   console.log('heroHours triggered');
+  //   var time = [];
+  //   for ( var i = 0; i < a.length; i++ ) {
+  //     time.push( b.a[i].time_played );
+  //   }
+  //   console.log(time);
+  //   return time;
+  // }
+
+  function heroHours(array, heroes) {
+    let time = [];
+    for (let i = 0; i < array.length; i++) {
+      if (heroes.hasOwnProperty(array[i])) {
+        heroes.array[i].time_played.push(time);
+      } else {
+        console.log('hit the else, something broke');
+      }
+    }
+  }
+
+
   // Templating with Handlebars
   Player.prototype.toHtml = function(type) {
     let template = Handlebars.compile($(`#${type}-view-template`).text());
