b0f52f3b4ed68923beacc57b22562046f56f288f
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-16 13:13:59 -0800

    chart font colors fixed

--- public/scripts/views/charts.js
+++ public/scripts/views/charts.js
@@ -2,6 +2,7 @@
 
 //Drawing a chart for a single user's stats
 function drawSingleChart(labels, values) { // eslint-disable-line
+  Chart.defaults.global.defaultFontColor = 'white'; // eslint-disable-line
   var ctx = document.getElementById('primaryChart').getContext('2d');
   new Chart(ctx,{ // eslint-disable-line
     type: 'horizontalBar',
@@ -19,15 +20,20 @@ function drawSingleChart(labels, values) { // eslint-disable-line
             'rgb(255,195,0)',
             'rgb(255,205,0)',
             'rgb(232,218,0)',
+            'rgb(255,255,20)',
             'rgb(192,255,0)',
             'rgb(68,232,0)',
             'rgb(0,255,35)',
             'rgb(0,255,112)',
+            'rgb(40,255,183)',
             'rgb(0,232,189)',
+            'rgb(7,239,228)',
+            'rgb(20,227,255)',
             'rgb(0,210,255)',
             'rgb(0,111,232)',
-            'rgb(0,33,255)',
             'rgb(0,80,255)',
+            'rgb(0,33,255)',
+            'rgb(86,20,255)',
             'rgb(131,0,232)',
             'rgb(206,0,255)',
             'rgb(232,0,195)',
@@ -63,14 +69,20 @@ function drawSingleChart(labels, values) { // eslint-disable-line
       animation: {
         duration: 1000,
         easing: 'easeOutBounce'
+      },
+      legend: {
+        fontColor: 'white',
+        labels: {
+          fontColor: 'white'
+        }
       }
     },
     scales: {
       yAxes: [{
         ticks: {
-          max: 300,
+          max: 30000,
           min: 0,
-          stepSize: 1.0
+          stepSize: 0.5,
         }
       }]
     }
@@ -79,6 +91,7 @@ function drawSingleChart(labels, values) { // eslint-disable-line
 
 //Drawing the comparison charts
 function drawCompareChart(labels, primaryValues, secondaryValues) { // eslint-disable-line
+  Chart.defaults.global.defaultFontColor = 'white'; // eslint-disable-line
   var ctx = document.getElementById('compareChart').getContext('2d');
   new Chart(ctx,{ // eslint-disable-line
     type: 'bar',
@@ -96,15 +109,20 @@ function drawCompareChart(labels, primaryValues, secondaryValues) { // eslint-di
             'rgb(255,195,0)',
             'rgb(255,205,0)',
             'rgb(232,218,0)',
+            'rgb(255,255,20)',
             'rgb(192,255,0)',
             'rgb(68,232,0)',
             'rgb(0,255,35)',
             'rgb(0,255,112)',
+            'rgb(40,255,183)',
             'rgb(0,232,189)',
+            'rgb(7,239,228)',
+            'rgb(20,227,255)',
             'rgb(0,210,255)',
             'rgb(0,111,232)',
-            'rgb(0,33,255)',
             'rgb(0,80,255)',
+            'rgb(0,33,255)',
+            'rgb(86,20,255)',
             'rgb(131,0,232)',
             'rgb(206,0,255)',
             'rgb(232,0,195)',
@@ -138,26 +156,31 @@ function drawCompareChart(labels, primaryValues, secondaryValues) { // eslint-di
           label: 'Them',
           data: secondaryValues,
           backgroundColor: [
-            'rgb(252, 128, 118)',
-            'rgb(249, 154, 119)',
-            'rgb(252, 182, 123)',
-            'rgb(252, 208, 133)',
-            'rgb(252, 224, 133)',
-            'rgb(252, 230, 138)',
-            'rgb(252, 245, 133)',
-            'rgb(223, 252, 133)',
-            'rgb(173, 252, 141)',
-            'rgb(138, 252, 154)',
-            'rgb(147, 249, 192)',
-            'rgb(143, 252, 232)',
-            'rgb(146, 233, 252)',
-            'rgb(146, 197, 252)',
-            'rgb(147, 160, 249)',
-            'rgb(151, 183, 252)',
-            'rgb(207, 152, 249)',
-            'rgb(235, 164, 252)',
-            'rgb(249, 167, 236)',
-            'rgb(249, 177, 201)'
+            'rgb(249, 168, 162)',
+            'rgb(249, 183, 159)',
+            'rgb(252, 204, 164)',
+            'rgb(252, 215, 153)',
+            'rgb(252, 231, 164)',
+            'rgb(249, 233, 164)',
+            'rgb(249, 245, 172)',
+            'rgb(252, 252, 169)',
+            'rgb(228, 249, 162)',
+            'rgb(195, 252, 171)',
+            'rgb(162, 249, 174)',
+            'rgb(153, 252, 197)',
+            'rgb(151, 252, 218)',
+            'rgb(152, 249, 231)',
+            'rgb(161, 252, 248)',
+            'rgb(155, 243, 255)',
+            'rgb(153, 235, 252)',
+            'rgb(156, 202, 252)',
+            'rgb(172, 196, 249)',
+            'rgb(184, 193, 252)',
+            'rgb(199, 179, 252)',
+            'rgb(222, 184, 252)',
+            'rgb(241, 183, 255)',
+            'rgb(252, 186, 241)',
+            'rgb(252, 184, 206)'
           ],
 
           hoverBackgroundColor: [
@@ -198,7 +221,7 @@ function drawCompareChart(labels, primaryValues, secondaryValues) { // eslint-di
           min: 0,
           stepSize: 0.5
         }
-      }]
+      }],
     }
   });
 }
